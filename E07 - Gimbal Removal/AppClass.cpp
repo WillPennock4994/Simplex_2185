ca8af1fa05bee548c23ef6f56a271136a5f9ac2e
Will <Will@WILLSPC>
2019-02-25 22:25:13 -0400

    E07 Deliverable

--- E07 - Gimbal Removal/AppClass.cpp
+++ E07 - Gimbal Removal/AppClass.cpp
@@ -25,26 +25,43 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 
-	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
-	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
-
-	//m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(1.0f));
-	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
 	
+	glm::quat xAxis = glm::angleAxis(glm::radians(m_v3Rotation.x), AXIS_X);//x
+
+	glm::quat yAxis = glm::angleAxis(glm::radians(m_v3Rotation.y), AXIS_Y);//y
+
+	glm::quat zAxis = glm::angleAxis(glm::radians(m_v3Rotation.z), AXIS_Z);//z
+
+	glm::quat orientation = xAxis * yAxis * zAxis;//Orientation of all axis
+
+	
+	m_v3Rotation.y = 0.0f;
+
+	m_v3Rotation.x = 0.0f;
+
+	m_v3Rotation.z = 0.0f;
+
+	
+	m_qOrientation = orientation * m_qOrientation;//set orientation
+
+	m_qOrientation = glm::normalize(m_qOrientation);//normalize vectors
+
+	
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
+
+
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
-	
+
 	//render list call
 	m_uRenderCallCount = m_pMeshMngr->Render();
 
 	//clear the render list
 	m_pMeshMngr->ClearRenderList();
-	
+
 	//draw gui
 	DrawGUI();
-	
+
 	//end the current frame (internally swaps the front and back buffers)
 	m_pWindow->display();
 }
